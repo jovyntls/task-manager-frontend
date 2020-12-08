2aac7d6ca9bbf17c776824a6e5a0ce083e52c836
jovyn <jovyn.tls@gmail.com>
2020-12-08 19:01:09 +0800

    add authentication and login form

---
+++ src/components/authentication/LoginForm.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { setUserSession } from "../../Utils/Common";
+
+function LoginForm(props) {
+	const [loading, setLoading] = useState(false);
+	const username = useFormInput("");
+	const password = useFormInput("");
+	const [error, setError] = useState(null);
+
+	// handle button click of login form
+	const handleLogin = () => {
+		setError(null);
+		setLoading(true);
+		axios
+			.post("http://localhost:3000/login", { username: username.value, password: password.value })
+			.then((response) => {
+				setLoading(false);
+				setUserSession(response.data.token, response.data.user);
+				props.history.push("/board");
+			})
+			.catch((error) => {
+				setLoading(false);
+				if (error.response.status === 401) setError(error.response.data.message);
+				else setError("Something went wrong. Please try again later.");
+			});
+	};
+
+	return (
+		<div>
+			Login
+			<br />
+			<br />
+			<div>
+				Username
+				<input type="text" {...username} />
+			</div>
+			<div style={{ marginTop: 10 }}>
+				Password
+				<input type="password" {...password} />
+			</div>
+			{error && (
+				<>
+					<small style={{ color: "red" }}>{error}</small>
+					<br />
+				</>
+			)}
+			<br />
+			<input type="button" value={loading ? "Loading..." : "Login"} onClick={handleLogin} disabled={loading} />
+			<br />
+		</div>
+	);
+}
+
+const useFormInput = (initialValue) => {
+	const [value, setValue] = useState(initialValue);
+
+	const handleChange = (e) => {
+		setValue(e.target.value);
+	};
+	return {
+		value,
+		onChange: handleChange,
+	};
+};
+
+export default LoginForm;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import App from "./App";
+import BoardView from "./components/BoardView";
+import LoginForm from "./components/authentication/LoginForm";
+import PrivateRoute from "./Utils/PrivateRoute";
+import PublicRoute from "./Utils/PublicRoute";
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/App" component={App} />
+				<PublicRoute exact path="/login" component={LoginForm} />
+				<PrivateRoute exact path="/board" component={BoardView} />
+				<App />
+			</Switch>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
