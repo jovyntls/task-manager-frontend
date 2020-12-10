d1d647c48643b0a797c051e06a3a19a6da58f88b
jovyn <jovyn.tls@gmail.com>
2020-12-11 01:52:21 +0800

    lint .jsx files

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -5,38 +5,38 @@ import PostService from "../services/PostService";
 class Task extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {title: this.props.data.title};
+		this.state = { title: this.props.data.title };
 	}
 	handleChange = (event) => {
-		this.setState({title: event.target.value});
-	}
+		this.setState({ title: event.target.value });
+	};
 	handleKeyDown = (event) => {
-		if (event.key === 'Enter') {
-			this.submitEdit()
+		if (event.key === "Enter") {
+			this.submitEdit();
 		}
-	}
+	};
 	submitEdit = () => {
-		console.log("saved: ", this.state.title)
-		PostService.editTaskTitle({id: this.props.data.id, title: this.state.title})
-		.then((response) => {
-			return response;
-		})
-		.catch((err) => console.log(err));
-	}
+		console.log("saved: ", this.state.title);
+		PostService.editTaskTitle({ id: this.props.data.id, title: this.state.title })
+			.then((response) => {
+				return response;
+			})
+			.catch((err) => console.log(err));
+	};
 	render() {
 		return (
 			<div>
-			<li>{this.props.data.title}</li>
-			<input 
-				type="text" 
-				className="form-control-plaintext" 
-				defaultValue={this.props.data.title}
-				onChange={this.handleChange}
-				onKeyDown={this.handleKeyDown}
-				onBlur={this.submitEdit}
+				<li>{this.props.data.title}</li>
+				<input
+					type="text"
+					className="form-control-plaintext"
+					defaultValue={this.props.data.title}
+					onChange={this.handleChange}
+					onKeyDown={this.handleKeyDown}
+					onBlur={this.submitEdit}
 				></input>
 			</div>
-		)
+		);
 	}
 }
 export default Task;
