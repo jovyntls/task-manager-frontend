55f8434dac2045f946ec016fe704d53138eec24c
jovyn <jovyn.tls@gmail.com>
2020-12-12 16:17:53 +0800

    add priority of uncreated tasks

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -4,6 +4,8 @@ import PostService from "../services/PostService";
 
 function NewTask(props) {
 	const [title, setTitle] = useState("");
+	const [priority, setPriority] = useState(0);
+
 	const handleChange = (event) => {
 		setTitle(event.target.value);
 	};
@@ -11,20 +13,26 @@ function NewTask(props) {
 		if (event.key === "Enter") {
 			submitEdit();
 			setTitle("");
+			setPriority(0);
 			props.refresher();
 		}
 	};
 	const submitEdit = () => {
 		console.log("saved: ", title);
-		PostService.addNewTask({ cat_id: props.cat_id, title: title })
+		PostService.addNewTask({ cat_id: props.cat_id, title: title, priority: priority })
 			.then((response) => {
 				return response;
 			})
 			.catch((err) => console.log(err));
 	};
+	const editPriority = () => {
+		setPriority((priority) => (priority + 1) % 3);
+	};
+
 	return (
 		<div>
 			<input type="checkbox"></input>
+			<button onClick={editPriority}>{priority}</button>
 			<input
 				type="text"
 				className="form-control-plaintext"
